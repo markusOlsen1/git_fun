bcad4bf070566267908e9f51b2959d3807b17f6c
Markus Olsen <markusditlevolsen@gmail.com>
2022-09-09 12:58:25 +0200

    new structure

---
+++ src/frontend/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import stepbutton from '../stepbutton';
+function App() {
+  return (
+    <><div> 
+      HELLO EVERYONE
+      stepbutton
+    </div>
+    <head>
+      <button>
+      </button>
+    </head>
+    </>
+  );
+}
+
+export default App;
---
+++ src/frontend/stepbutton.tsx
@@ -0,0 +1,5 @@
+import './stepbutton.css'
+function Stepbutton(props){
+    return (<><div> props.text </div></>
+)};
+export default Stepbutton;
